f0d8d25030289e796602c304c39b433919c11beb
Jim Heising <jheising@redfoundry.com>
2014-07-03 12:02:33 -0700

    initial work for grunt integration

---
+++ Gruntfile.js
@@ -0,0 +1,25 @@
+module.exports = function(grunt) {
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+        concat: {
+            css: {
+                src: [
+                    'css/*'
+                ],
+                dest: 'combined.css'
+            },
+            js : {
+                src : [
+                    'js/*'
+                ],
+                dest : 'combined.js'
+            }
+        },
+    });
+
+    grunt.loadNpmTasks('grunt-contrib-concat');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-contrib-cssmin');
+    grunt.registerTask('default', [ 'concat:css', 'cssmin:css', 'concat:js', 'uglify:js' ]);
+};
