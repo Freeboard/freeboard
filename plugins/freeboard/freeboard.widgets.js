ec81b98c049baafc7b48bb4563256c67d047c7a5
Bernard Tai <10083575+bernietai@users.noreply.github.com>
2021-03-21 12:59:14 -0400

    Added OS dark mode preference support

--- plugins/freeboard/freeboard.widgets.js
+++ plugins/freeboard/freeboard.widgets.js
@@ -59,12 +59,13 @@
 		element.empty().append(legendElt);
 
 		freeboard.addStyle('.sparkline-legend', "margin:5px;");
-		freeboard.addStyle('.sparkline-legend-value',
-			'color:white; font:10px arial,san serif; float:left; overflow:hidden; width:50%;');
-		freeboard.addStyle('.sparkline-legend-value span',
-			'font-weight:bold; padding-right:5px;');
-	}
 
+        //BT Default label to black color
+		freeboard.addStyle('.sparkline-legend-value','color:#000; font:10px arial,san serif; float:left; overflow:hidden; width:50%;');
+        freeboard.addStyle('@media (prefers-color-scheme: dark)',".sparkline-legend-value{color:#fff;}"
+        );
+	}
+ 
 	function addValueToSparkline(element, value, legend) {
 		var values = $(element).data().values;
 		var valueMin = $(element).data().valueMin;
